7c9b5a708681041119cfc1a6ec6239fc8af2f9ad
Ruslan Mykhailenko PC <mihailencko.r222@gmail.com>
2021-08-10 06:18:10 +0300

    Revert "minor changes"
    This reverts commit 640a5b9b1fb90b20d8db7b572878dd6dfccac3ed.

--- handlers/commands_reply.py
+++ handlers/commands_reply.py
@@ -8,6 +8,4 @@ import lang
 
 @dp.message_handler(commands=["report"], is_reply=True)
 async def userReport(message: types.Message):
-	pg.updateUserCounter(message.from_user.id, message.chat.id)
-
 	return
