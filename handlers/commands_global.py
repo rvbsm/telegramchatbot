716c03f80d280c4d2015d0063ab893f36925d9d6
Ruslan Mykhailenko PC <mihailencko.r222@gmail.com>
2021-08-10 06:14:28 +0300

    minor changes

--- handlers/commands_global.py
+++ handlers/commands_global.py
@@ -8,6 +8,8 @@ import lang
 
 @dp.message_handler(commands=["help"])
 async def helpMessage(message: types.Message):
+	pg.updateUserCounter(message.from_user.id, message.chat.id)
+
 	chatLang = pg.getChatLang(message.chat.id)
 
 	if pg.getCommandsList(message.chat.id):
@@ -15,10 +17,12 @@ async def helpMessage(message: types.Message):
 	else:
 		await message.reply(text=getattr(lang, chatLang).helpNoCommandsText)
 
-
 @dp.message_handler(commands=["stat"])#, is_reply)
 async def userStats(message: types.Message):
+	pg.updateUserCounter(message.from_user.id, message.chat.id)
+
 	chatLang = pg.getChatLang(message.chat.id)
+
 	pg.getUserWarn(message.from_user.id, message.chat.id)
 	await message.reply(text=getattr(lang, chatLang).aboutUserText.format(
 		message.from_user.first_name,
